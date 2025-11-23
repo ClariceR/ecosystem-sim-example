9565e6acbf284c28ab58d8ac30959cd31890304f
Clarice Dall'Orto <clarirad@clarices-mbp.mynet>
2025-11-23 12:54:02 +0000

    Fix chicken. Add nature

--- src/main.rs
+++ src/main.rs
@@ -22,16 +22,52 @@ fn setup(
 
     // Chicken Model
     commands.spawn(SceneRoot(
-        asset_server.load(GltfAssetLabel::Scene(0).from_asset("models/animated/chicken.glb")),
+        asset_server.load(GltfAssetLabel::Scene(0).from_asset("models/animated/chicken2.glb")),
     ));
 
     // Rock
     commands.spawn((
         SceneRoot(asset_server.load(
-            GltfAssetLabel::Scene(0).from_asset("models/rocks/rock3/Rock_3_H_Color1.glb"),
+            GltfAssetLabel::Scene(0).from_asset("models/nature/Rock_3_H_Color1.glb"),
         )),
         Transform::from_xyz(1.5, 0.2, 1.5)
             .with_scale(Vec3::new(0.5, 0.5, 0.5))
+    ));
+
+    // Grass
+    commands.spawn((
+        SceneRoot(asset_server.load(
+            GltfAssetLabel::Scene(0).from_asset("models/nature/grass1and2.glb"),
+        )),
+        Transform::from_xyz(0.8, 0.0, 1.6)
+            //.with_scale(Vec3::new(0.5, 0.5, 0.5))
+    ));
+
+    // Grass 2
+    commands.spawn((
+        SceneRoot(asset_server.load(
+            GltfAssetLabel::Scene(0).from_asset("models/nature/grass4.glb"),
+        )),
+        Transform::from_xyz(2.0, 0.0, 1.9)
+            .with_scale(Vec3::new(0.7, 0.7, 0.7))
+    ));
+
+    // Bush
+    commands.spawn((
+        SceneRoot(asset_server.load(
+            GltfAssetLabel::Scene(0).from_asset("models/nature/bush1default.glb"),
+        )),
+        Transform::from_xyz(-1.2, 0.0, -1.6)
+            //.with_scale(Vec3::new(0.6, 0.6, 0.6))
+    ));
+
+    // Tree
+    commands.spawn((
+        SceneRoot(asset_server.load(
+            GltfAssetLabel::Scene(0).from_asset("models/nature/treeBdefault.glb"),
+        )),
+        Transform::from_xyz(1.2, 0.0, -1.6)
+            //.with_scale(Vec3::new(0.6, 0.6, 0.6))
     ));
 
 
@@ -41,13 +77,13 @@ fn setup(
             shadows_enabled: true,
             ..default()
         },
-        Transform::from_xyz(-4.0, 8.0, -4.0),
+        Transform::from_xyz(4.0, 8.0, 4.0),
     ));
 
     // Camera
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(-2.0, 4.5, -5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(2.0, 4.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 
 }
